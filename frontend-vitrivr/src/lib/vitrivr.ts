dcad539ee914291ff8afd593d1456b66cdbfb064
Andrina Geller <andrina.geller@unibas.ch>
2025-09-08 13:31:06 +0200

    add new api (query)

--- frontend-vitrivr/src/lib/vitrivr.ts
+++ frontend-vitrivr/src/lib/vitrivr.ts
@@ -3,26 +3,38 @@ export const API_BASE = import.meta.env.VITE_VITRIVR_BASE_URL || "http://localho
 export const videoUrl = (id: string) => `${API_BASE}/api/${SCHEMA}/fetch/video/${encodeURIComponent(id)}`;
 export const thumbnailUrl = (id: string) => `${API_BASE}/api/${SCHEMA}/fetch/thumbnail/${encodeURIComponent(id)}`;
 
+/**
+ * Builds the query for vitrivr-engine for textual queries
+ * @param prompt
+ */
 export function buildTextQuery(prompt: string) {
     return {
-        inputs: {"input-text": {type: "TEXT", data: prompt}},
-        "operations": {
-            "clip": {"type": "RETRIEVER", "field": "clip", "input": "input-text"},
-            "relations": {"type": "TRANSFORMER", "transformerName": "RelationExpander", "input": "clip"},
-            "lookup": {"type": "TRANSFORMER", "transformerName": "FieldLookup", "input": "relations"},
-            "aggregator": {"type": "TRANSFORMER", "transformerName": "ScoreAggregator", "input": "lookup"},
-            "filelookup": {"type": "TRANSFORMER", "transformerName": "FieldLookup", "input": "aggregator"}
+        "inputs": {
+            "txt": {"type": "TEXT", "data": prompt}
         },
-        "context": {
-            "global": {
-                "limit": "1000"
+        "operations": {
+            "clip": {"field": "clip", "inputs": {"txt": "txt"}, "parameters": {"limit": "1000"}},
+            "relations": {
+                "factory": "RelationExpander",
+                "inputs": {"in": "clip"},
+                "parameters": {"outgoing": "partOf"}
             },
-            "local": {
-                "lookup": {"field": "time", "keys": "start, end"},
-                "relations": {"outgoing": "partOf"},
-                "filelookup": {"field": "file", "keys": "path"}
+            "aggregator": {"factory": "ScoreAggregator", "inputs": {"in": "relations"}},
+            "timelookup": {
+                "factory": "FieldLookup",
+                "inputs": {"in": "aggregator"},
+                "parameters": {"field": "time", "keys": "start, end"}
+            },
+            "filelookup": {
+                "factory": "FieldLookup",
+                "inputs": {"in": "timelookup"},
+                "parameters": {"field": "file", "predicates": "partOf", "keys": "path"}
             }
         },
-        "output": "filelookup",
+        "output": "filelookup"
     } as const;
 }
+
+/**
+ * TODO: Add more serialization things here. For example image-to-image stuff and the corresponding base64 encoding.
+ */
