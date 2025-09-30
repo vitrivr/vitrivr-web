beefabe8555e5ccdfb3f017b12a735a49d3c82f6
Andrina Geller <andrina.geller@unibas.ch>
2025-09-30 11:07:48 +0200

    add case handling for unimplemented modalities in querybuilder

--- frontend-vitrivr/src/lib/vitrivr.ts
+++ frontend-vitrivr/src/lib/vitrivr.ts
@@ -17,77 +17,69 @@ export function buildTemporalQuery(blocks: BlockState[]) {
     const textBits: string[] = [];
 
     for (const b of blocks) {
-        //const isEmotion = b.modality === "emotions";
+        const isCLIP = b.modality === "clip";
+        const isImage = b.queryType === "image";
         const isText = b.queryType === "text";
+        const isEmotion = b.emotion !== undefined;
+        console.log(b.emotion)
 
-        if (isText) {
+        if (isCLIP && isImage) {
+            // TODO Implement image-to-image
+            console.log("The image case has not been implemented in the backend yet. ")
+        } else if (isEmotion) {
+            // TODO Implement emotions
+            console.log("The emotion case has not been implemented in the backend yet. ")
+        } else if (isCLIP && isText) {
             textBits.push(b.textQuery);
+            const inputs: Inputs = Object.fromEntries(textBits.map((term, i) => [`t${i}`, {
+                type: "TEXT",
+                data: term
+            } as TextInput,])); // maps all the entries of the prompt array to variable called t1,...,tn
+            const clipOps: Operator = Object.fromEntries(
+                textBits.map((_, i) => {
+                    const opName = i === 0 ? "clip" : `clip${i}`; // first one is just "clip" and not "clip0"
+                    const op = {
+                        field: "clip",
+                        inputs: {txt: `t${i}`},
+                    } as any;
+                    return [opName, op];
+                })
+            );
+
+            const temporalInputs = Object.fromEntries(textBits.map((_, i) => [`in${i}`, i === 0 ? "clip" : `clip${i}`]));
+            const operations: Operator = {
+                ...clipOps,
+                "temporal-aggregator": {
+                    factory: "TemporalSequenceAggregator",
+                    inputs: temporalInputs,
+                },
+                relations: {
+                    factory: "RelationExpander",
+                    inputs: {in: "temporal-aggregator"},
+                    parameters: {outgoing: "partOf"},
+                },
+                aggregator: {
+                    factory: "ScoreAggregator",
+                    inputs: {in: "relations"},
+                },
+                timelookup: {
+                    factory: "FieldLookup",
+                    inputs: {in: "aggregator"},
+                    parameters: {field: "time", keys: "start, end"},
+                },
+                filelookup: {
+                    factory: "ObjectFieldLookup",
+                    inputs: {in: "timelookup"},
+                    parameters: {field: "file", predicates: "partOf", keys: "path"},
+                },
+            };
+            return {
+                inputs,
+                operations,
+                output: "filelookup",
+            };
         }
-
-        /**if (isEmotion && b.textQuery.trim()) {
-         textBits.push(`${b.textQuery.trim()} ${b.emotion ?? ""}`.trim());
-         continue;
-         }
-
-         if (!isEmotion && isText && b.textQuery.trim()) {
-         textBits.push(b.textQuery.trim());
-         continue;
-         }**/
-
     }
-    if (textBits.length === 0) {
-        throw new Error("Prompt Array must not be empty.");
-    }
-    const inputs: Inputs = Object.fromEntries(textBits.map((term, i) => [`t${i}`, {
-        type: "TEXT",
-        data: term
-    } as TextInput,])); // maps all the entries of the prompt array to variable called t1,...,tn
-
-    const clipOps: Operator = Object.fromEntries(
-        textBits.map((_, i) => {
-            const opName = i === 0 ? "clip" : `clip${i}`; // first one is just "clip" and not "clip0"
-            const op = {
-                field: "clip",
-                inputs: {txt: `t${i}`},
-            } as any;
-            return [opName, op];
-        })
-    );
-
-    const temporalInputs = Object.fromEntries(textBits.map((_, i) => [`in${i}`, i === 0 ? "clip" : `clip${i}`]));
-
-    const operations: Operator = {
-        ...clipOps,
-        "temporal-aggregator": {
-            factory: "TemporalSequenceAggregator",
-            inputs: temporalInputs,
-        },
-        relations: {
-            factory: "RelationExpander",
-            inputs: {in: "temporal-aggregator"},
-            parameters: {outgoing: "partOf"},
-        },
-        aggregator: {
-            factory: "ScoreAggregator",
-            inputs: {in: "relations"},
-        },
-        timelookup: {
-            factory: "FieldLookup",
-            inputs: {in: "aggregator"},
-            parameters: {field: "time", keys: "start, end"},
-        },
-        filelookup: {
-            factory: "ObjectFieldLookup",
-            inputs: {in: "timelookup"},
-            parameters: {field: "file", predicates: "partOf", keys: "path"},
-        },
-    };
-
-    return {
-        inputs,
-        operations,
-        output: "filelookup",
-    };
 }
 
 /**
@@ -121,7 +113,3 @@ export function buildTextQuery(prompt: string) {
         "output": "filelookup"
     } as const;
 }
-
-/**
- * TODO: Add more serialization things here. For example image-to-image stuff and the corresponding base64 encoding.
- */
