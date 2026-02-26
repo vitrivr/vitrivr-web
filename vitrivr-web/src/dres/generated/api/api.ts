4c399aa2d91f39a97349193d7a4bbed8914c90fb
Andrina Geller <andrina.geller@unibas.ch>
2026-02-26 16:00:59 +0100

    final tweaks to the README.md and removal of unused imports in generated files

--- vitrivr-web/src/dres/generated/api/api.ts
+++ vitrivr-web/src/dres/generated/api/api.ts
@@ -21,10 +21,6 @@ import globalAxios from 'axios';
 import {
     DUMMY_BASE_URL,
     assertParamExists,
-    setApiKeyToObject,
-    setBasicAuthToObject,
-    setBearerAuthToObject,
-    setOAuthToObject,
     setSearchParams,
     serializeDataIfNeeded,
     toPathString,
